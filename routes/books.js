63e0a631894285d43f1228fca6a2d9d39ad620f8
albert <albertdanieln@yahoo.com>
2017-09-08 00:54:23 +0700

    server: update server.js for crud operation gitignore: enhance spacing

--- routes/books.js
+++ routes/books.js
@@ -16,9 +16,48 @@ router.get('/books', function(req,res,send){
         if(err){
             res.send(err);
         }else{
-            res.json(books);            
+            res.json(books);
         }
     })
+})
+
+router.get('/book/:id', function(req, res, send){
+    db.book.find({_id: mongojs.ObjectId(req.params.id)}, function(err, book){
+        if(err){
+            res.send(err);
+        }else{
+            res.json(book);
+        }
+    })
+})
+
+router.post('/book', function(req,res,send){
+    var book = req.body;
+    if(!book.title){
+        res.status(400);
+        res.json({
+            "error": "Bad Data"
+        });
+    }else{
+        db.book.save(book, function(err, book){
+            if(err){
+                res.send(err);            
+            }else{
+                res.json(book);
+            }
+        });
+    }
+})
+
+router.delete('/book', function(req, res, send){
+    var id = req.query.id
+    db.book.remove({_id: mongojs.ObjectId(id)} , function(err, book){
+        if(err){
+            res.send(err);
+        }else{
+            res.json(book);
+        }
+    });
 })
 
 module.exports = router;
